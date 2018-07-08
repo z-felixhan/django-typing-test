37e37bd4de5f9e188cd91e78bb88f37267f0b8db
z-felixhan <34609664+z-felixhan@users.noreply.github.com>
2018-07-07 21:00:04 -0400

    Added Vue method that stores the words in the test into an array. Also included v-model of user input

--- assets/js/type.js
+++ assets/js/type.js
@@ -2,6 +2,19 @@ var app = new Vue({
     delimiters: ['[[', ']]'],
     el: '#app',
     data: {
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.!'
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.!',
+      input: ''
+    },
+    methods: {
+      check: function() {
+        var arr = this.text.split(" ");
+        document.getElementById("demo").innerHTML = arr;
+      }
     }
   })
+
+function useCheck() {
+  app.check();
+}
+
+useCheck();
